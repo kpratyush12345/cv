ce088c79fd2d9653bc1e23e7e43ada2cbc7bb44e
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2020-12-27 16:48:20 +0530

    Update index.js

--- dicegame/index.js
+++ dicegame/index.js
@@ -12,7 +12,7 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png" ;
 
-var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource);;
+var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);;
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML ="Player 1 Wins";
